d84d1e0245bbf3aae033639b1df44909c125e42c
peterlowrance <peter.lowrance@gmail.com>
2019-02-28 18:45:54 -0500

    classes load on click

--- readLessons.js
+++ readLessons.js
@@ -1,4 +1,12 @@
 function loadLesson(){
-    var allLessons = JSON.parse(lessons);
-    window.alert(allLessons);
+    var lessonName;
+    const fs = require('fs');
+    let rawdata = fs.readFileSync('./data/lessons/lessons.json');
+    let allLessons = JSON.parse(rawdata);
+    allLessons.forEach(function(element){
+        if(element.id == 1){
+            lessonName = `<h>${element.name}</h>`;
+        }
+    });
+    document.getElementById("button1").innerHTML = lessonName;
 }
