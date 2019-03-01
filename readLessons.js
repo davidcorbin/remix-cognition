aed7d8abb50477f3cb9db86ab99deb3bf38fbe9f
Peter <peter.lowrance@gmail.com>
2019-02-28 21:25:42 -0500

    updated formatting

--- readLessons.js
+++ readLessons.js
@@ -1,12 +1,12 @@
-function loadLesson(){
-    var lessonName;
-    const fs = require('fs');
-    let rawdata = fs.readFileSync('./data/lessons/lessons.json');
-    let allLessons = JSON.parse(rawdata);
-    allLessons.forEach(function(element){
-        if(element.id == 1){
-            lessonName = `<h>${element.name}</h>`;
-        }
-    });
-    document.getElementById("button1").innerHTML = lessonName;
+function loadLesson () {
+  var lessonName
+  const fs = require('fs')
+  let rawdata = fs.readFileSync('./data/lessons/lessons.json')
+  let allLessons = JSON.parse(rawdata)
+  allLessons.forEach(function (element) {
+    if (element.id === 1) {
+      lessonName = `<h>${element.name}</h>`
+    }
+  })
+  document.getElementById('button1').innerHTML = lessonName
 }
