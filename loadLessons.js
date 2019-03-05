a00673315574fb208fe6a616843763fedff2ef9d
Mitchell Harvey <mph121@users.noreply.github.com>
2019-03-05 17:14:21 -0500

    Changed format of displaying lessons

--- loadLessons.js
+++ loadLessons.js
@@ -4,10 +4,10 @@ this.onload = function () {
   let allLessons = JSON.parse(rawdata)
   const ol = document.getElementById('lessons')
   allLessons.forEach(function (element) {
-    const li = document.createElement('li')
-    const listButton = document.createElement('button')
+    const li = document.createElement('div')
+    const listButton = document.createElement('a')
     listButton.onclick = function () {
-      window.location.href = 'lesson.html' + '#' + element.name
+      window.location.href = 'lesson.html' + '#' + element.id
     }
     listButton.innerHTML = element.name
     li.appendChild(listButton)
