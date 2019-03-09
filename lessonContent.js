0f0e324cf1d1a33b313ebe5cb3ba8802c1aeb5b9
David Corbin <daconex@gmail.com>
2019-03-09 18:04:27 -0500

    Adjusted lesson page style and added iframes to lesson page

--- lessonContent.js
+++ lessonContent.js
@@ -13,6 +13,7 @@ function lessonContent () {
   store.set('started', started)
   // document.getElementById('lessonContent').innerHTML = lessonName
   const ol = document.getElementById('lessonPages')
+
   // file I/O
   const fs = require('fs')
   let rawdata = fs.readFileSync('./data/lessons/lessons.json')
@@ -20,15 +21,17 @@ function lessonContent () {
   allLessons.forEach(function (element) {
     if (element.id.toString() === lessonID) {
       document.getElementById('lessonContent').innerHTML = element.name
-      element.sources.forEach(function (source) {
-        const li = document.createElement('li')
-        const sourceLabel = document.createElement('label')
-        sourceLabel.innerHTML = source.title
-        const listButton = document.createElement('button')
-        listButton.innerHTML = source.url
-        li.appendChild(sourceLabel)
-        li.appendChild(listButton)
-        ol.appendChild(li)
+      element.sources.forEach(function (source, i) {
+        const ifram = document.createElement('iframe')
+        ifram.src = source.url
+
+        const individualLessonDiv = document.createElement('div')
+        individualLessonDiv.className = 'individualLesson'
+        const lessonTitle = document.createElement('h3')
+        lessonTitle.innerText = i + 1 + '. ' + source.title
+        individualLessonDiv.append(lessonTitle)
+        individualLessonDiv.append(ifram)
+        ol.append(individualLessonDiv)
       })
     }
   })
